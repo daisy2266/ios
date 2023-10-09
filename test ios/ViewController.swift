4d2580f6df9aa9868e1575b536afcd0aa92c6690
Daria Butolina <butolinad@yandex.ru>
2023-10-09 18:58:30 +0300

    added project

---
+++ test ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test ios
+//
+//  Created by Дарья Бутолина on 02.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
