782a0f779b3b5e2d9da43fd35059918ef2f5f6a0
Daria Butolina <butolinad@yandex.ru>
2023-10-16 19:44:28 +0300

    added url session

--- test ios/ViewController.swift
+++ test ios/ViewController.swift
@@ -12,7 +12,24 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
-        // Do any additional setup after loading the view.
+        let url: URL = URL (string: "https://api.punkapi.com/v2/beers")!
+        //восклиц - говорим системе что точно не ноль
+        URLSession.shared.dataTask(with: url, completionHandler:  { data, response, error in
+            guard
+                let data = data,
+                let response,
+                error == nil
+            else {
+                return
+            }
+            
+            let str = String(data: data, encoding: .utf8)
+            print ("Полученные данные: \(str ?? "")")
+            
+        }) .resume()
+        
+        
+        
     }
 
 
